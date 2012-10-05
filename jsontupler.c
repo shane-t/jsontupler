03bc59987a176ec1d595278f3ff4721b6afa243f
Shane T Odlum <shanet@shane.(none)>
2012-10-05 10:19:56 +0100

    Reformatting

--- jsontupler.c
+++ jsontupler.c
@@ -5,7 +5,7 @@
 #define false 0
 #define true 1
 
-int main (int argc, char* argv[]) {
+int main ( int argc, char* argv[] ) {
 
     FILE * fp;
     int newfile = false;
@@ -18,23 +18,23 @@ int main (int argc, char* argv[]) {
 
 
     if ( argc < 2 ) {
-        printf("Usage: %s [output filename]\n", argv[0]);
+        printf( "Usage: %s [output filename]\n", argv[0] );
         exit(0);
     }
 
     fp = fopen( argv[1], "r");
 
     if ( fp == NULL ) {
-        fputs("- creating new file.\n", stdout);
+        fputs( "- creating new file.\n", stdout );
         newfile = true;
     } else {
         fclose(fp);
     }
 
-    fp = fopen( argv[1], "a+");
+    fp = fopen( argv[1], "a+" );
     
-    if (newfile) {
-        fputs("- beginning JSON\n", stdout);
+    if ( newfile ) {
+        fputs( "- beginning JSON\n", stdout );
         fwrite("[\n", 1, 1, fp);
         first = true;
     } else {
@@ -43,41 +43,41 @@ int main (int argc, char* argv[]) {
 
     for (;;) {
 
-        memset(buffer, 0, sizeof buffer);
+        memset( buffer, 0, sizeof buffer );
 
-        fputs("Key?> ",stdout);
-        fflush(stdout);
-        fgets(input, sizeof input, stdin);
+        fputs( "Key?> ", stdout );
+        fflush( stdout );
+        fgets( input, sizeof input, stdin );
         
         // outta here
 
-        if (strcmp(input, "END\n") == 0) {
-            fputs("Written to disk\n", stdout);
-            fflush(stdout);
+        if ( strcmp( input, "END\n" ) == 0 ) {
+            fputs( "Written to disk\n", stdout );
+            fflush( stdout );
             break;
         }
 
-        if (input[strlen(input) - 1] == '\n') {
+        if ( input[strlen(input) - 1] == '\n' ) {
             input[strlen(input) - 1] = '\0';
         }
 
-        if (first) {
+        if ( first ) {
             //not first anymore, next time, there's a comma
             first = false;
-            strcat (buffer, "  ");
+            strcat( buffer, "  " );
         } else {
-            strcat (buffer, "  ,");
+            strcat( buffer, "  ," );
         }
 
         strcat( buffer, " { \"" );
         strcat( buffer, input );
         strcat( buffer, "\": " );
 
-        fputs("Value?> ",stdout);
-        fflush(stdout);
-        fgets(input, sizeof input, stdin);
+        fputs( "Value?> ",stdout );
+        fflush( stdout );
+        fgets( input, sizeof input, stdin );
 
-        if (input[strlen(input) - 1] == '\n') {
+        if ( input[strlen(input) - 1] == '\n' ) {
             input[strlen(input) - 1] = '\0';
         }
 
@@ -85,36 +85,36 @@ int main (int argc, char* argv[]) {
         strcat( buffer, input );
         strcat( buffer, "\" }\n" );
 
-        fputs (buffer, stdout);
-        fputs (buffer, fp);
+        fputs( buffer, stdout );
+        fputs( buffer, fp );
     }
 
     answered = false;
 
     do {
-        fputs ("Finalize? (Y/N)> ", stdout);
-        fflush(stdout);
+        fputs( "Finalize? (Y/N)> ", stdout );
+        fflush( stdout );
 
-        fgets(input, sizeof input, stdin);
+        fgets( input, sizeof input, stdin );
 
-        if (input[0] == 'Y') {
+        if ( input[0] == 'Y' ) {
             answered = true;
             finalize = true;
-        } else if (input[0] == 'N') {
+        } else if (input[0] == 'N' ) {
             answered = true;
             finalize = false;
         }
 
-    } while (answered == false);
+    } while ( answered == false );
 
     if ( finalize ) {
-        fputs("Finalizing...\n", stdout);
-        fwrite("]", 1, 1, fp);
+        fputs( "Finalizing...\n", stdout );
+        fwrite( "]", 1, 1, fp );
     } else {
-        fputs("Not Finalizing\n", stdout);
+        fputs(  "Not Finalizing\n", stdout );
     }
 
-    fclose(fp);
+    fclose( fp );
     return 0;
 }
 
